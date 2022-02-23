e4b656e2c2356dedceae0683f4287f8db21e94b4
Maksim <maximaa2001@gmail.com>
2022-02-23 20:50:11 +0300

    added all fikes

--- Doudle jump/Bullet.h
+++ Doudle jump/Bullet.h
@@ -2,7 +2,6 @@
 #include "Entity.h"
 class Bullet: public Entity
 {
-	
 
 public:
 	Bullet(float x, float y, float width, float height, const WCHAR* image);
--- Doudle jump/Platform.cpp
+++ Doudle jump/Platform.cpp
@@ -1,6 +1,5 @@
 #include "Platform.h"
 
-
 Platform::Platform(float x, float y, float width, float height, const WCHAR* image):Entity(x, y, width, height, image) {
 	
 }
